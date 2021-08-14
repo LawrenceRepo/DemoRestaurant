bc9bf833324bc48767fc37d7c389b719b6eb1840
LawrenceRepo <https://github.com/LawrenceRepo?tab=repositories>
2021-08-14 14:12:51 +0200

    packaged changed

---
+++ src/main/java/DemoRestaurant.java
@@ -0,0 +1,13 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestaurant {
+
+	public static void main(String[] args) { 
+		SpringApplication.run(DemoRestaurant.class, args);
+	}
+
+}
