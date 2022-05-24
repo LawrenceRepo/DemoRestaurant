4341d10aa9d3cd94b373a730ae9f62687808754f
anumu <a@test.c>
2022-05-24 12:50:23 +0200

    added annotations

--- src/main/java/DemoRestaurant.java
+++ src/main/java/DemoRestaurant.java
@@ -2,8 +2,14 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.lawrence")
+@EnableJpaRepositories(basePackages = "com.lawrence")
+@EntityScan(basePackages = "com.lawrence")
 public class DemoRestaurant {
 
 	public static void main(String[] args) { 
